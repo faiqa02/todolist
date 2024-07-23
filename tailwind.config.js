de3b9a45e0f8ccd195f5c697f184893ad0e9e5bf
faiqa asghar <faiqa@zaffretech.co>
2024-07-23 13:26:53 +0500

    add the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,19 @@ export default {
     "./src/**/*.{html, js, ts, vue}", 
     "./src/**/*"
 ],
-  theme:[],
+  theme: {
+  
+    extend: {
+      colors: {
+        transparentBlack:"rgba(0,0,0,0.85)",
+      }
+    },
     screens:{
       xs:"480px",
       sm:"768px",
       md:"1060px"
     },
+  },
   plugins: [],
 }
 
